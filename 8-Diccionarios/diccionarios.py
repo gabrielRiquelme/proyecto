99b2405f842ef19d36f447712bfbf4544fcf4307
JuanR <gabrielcabj986@gmail.com>
2024-05-17 18:27:49 -0300

    Dic

--- 8-Diccionarios/diccionarios.py
+++ 8-Diccionarios/diccionarios.py
@@ -1,3 +1,3 @@
 dic = {'Usuario' : "JuanR", 'Contra' : 12345}
 
-print(dic )
+print(dic)
---
+++ 8-Diccionarios/elementoDic.py
@@ -0,0 +1,14 @@
+dic = {'Nombre':'Juan' , 'Edad' : 21, 'Estatura' : 1.70 }
+
+print(dic)
+print(dic.keys())
+print(dic.values())
+print(dic['Estatura'])
+
+dic['Peso'] = 63  #Agregamos llave y valor.
+
+print(dic)
+
+dic['Nombre'] = "Gabriel"  #Modificamos y valor.
+
+print(dic)
