171ae7450c3f2bcbd4894ae87311d6394d08ae71
Juan R <gabrielcabj986@gmail.com>
2024-05-22 15:07:28 -0300

    Ejer1 y2

---
+++ 11-bucles/ejer1rangos.py
@@ -0,0 +1,8 @@
+for i in range(1,11):
+    print(i)
+
+num1 = int(input('Ingrese un numero para calcular el rango: '))
+num2 =int(input('Ingrese un numero para calcular el rango, con el numero anterior: '))
+
+for i in range(num1,num2+1):
+    print(i)
---
+++ 11-bucles/ejr2rangos.py
@@ -0,0 +1,6 @@
+num1 = int(input('Ingrese un numero para calcular el rango: '))
+num2 =int(input('Ingrese un numero para calcular el rango, con el numero anterior: '))
+
+for i in range(num1,num2,1):
+    if i % 2 == 0:
+        print(i)
