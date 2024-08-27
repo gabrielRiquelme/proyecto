ff2bb4b381c58c5f0258166f27397507fcce8e21
Juan R <gabrielcabj986@gmail.com>
2024-08-27 18:13:33 -0300

    tkinter

---
+++ Capacitacion3_Django/InterfazGrafica/interfaz.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk() #Declaro ventana#
+
+root.title('Riki')
+
+root.resizable(0,0) #Manejo de extension de ventan#
+
+root.iconbitmap("lm10.ico")
+
+
+
+
+root.mainloop() #Sentencia para que queda abierta la ventana#
