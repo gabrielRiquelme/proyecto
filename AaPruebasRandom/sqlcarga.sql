51972adbc2dda646759a9d363981549819dbb87a
Jr <gabrielcabj986@gmail.com>
2024-09-08 18:09:46 -0300

    SQL

---
+++ AaPruebasRandom/sqlcarga.sql
@@ -0,0 +1,6 @@
+insert into pruebas_json(informacion) values('{"nombre":"Luis","edad":31,"ciudad":"san jose"}');
+
+SELECT * FROM pruebas_json
+
+--CREAR BASE
+CREATE DATABASE tienda2 DEFAULT CHARACTER SET utf8mb4;
